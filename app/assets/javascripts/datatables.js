471d02b82104a3943e2ba5e63b26617d82552dc1
Jose Boronowski <jlboronowskidev@gmail.com>
2020-10-12 11:33:29 -0300

    Añadido rutas de positivos avanzado al layout

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -8,13 +8,13 @@
 //= require datatables/extensions/Buttons/buttons.print
 //= require datatables/extensions/Buttons/buttons.colVis
 //= require datatables/extensions/Buttons/buttons.flash
-//= require datatables/extensions/ColReorder/dataTables.colReorder
-//= require datatables/extensions/FixedColumns/dataTables.fixedColumns
-//= require datatables/extensions/FixedHeader/dataTables.fixedHeader
+// require datatables/extensions/ColReorder/dataTables.colReorder
+// require datatables/extensions/FixedColumns/dataTables.fixedColumns
+// require datatables/extensions/FixedHeader/dataTables.fixedHeader
 // require datatables/extensions/KeyTable/dataTables.keyTable
-//= require datatables/extensions/Responsive/dataTables.responsive
-//= require datatables/extensions/RowGroup/dataTables.rowGroup
-//= require datatables/extensions/RowReorder/dataTables.rowReorder
+// require datatables/extensions/Responsive/dataTables.responsive
+// require datatables/extensions/RowGroup/dataTables.rowGroup
+// require datatables/extensions/RowReorder/dataTables.rowReorder
 // require datatables/extensions/Scroller/dataTables.scroller
 // require datatables/extensions/Select/dataTables.select
 
